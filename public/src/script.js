ed849662e8302e9deb98038474ebffdb757bf9a5
Rattana <rattana.pheak.rp@gmail.com>
2022-12-14 13:25:25 +0100

    Update Changes API Img 2

--- public/src/script.js
+++ public/src/script.js
@@ -1,38 +1,47 @@
-
-let content = document.querySelector('#content');
-
+let content = document.querySelector("#content");
 
 async function getFetch() {
   for (let i = 0; i < 5; i++) {
-    let getData = await fetch('https://www.themealdb.com/api/json/v1/1/random.php');
+    let getData = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
     let showData = await getData.json();
 
-    // if(showData.meals[0].strCategory != "Dessert"){
-    // console.log(showData.meals[0].strCategory);
-    // }
+    if (showData.meals[0].strCategory != "Dessert") {
+      // showData.meals[0];
 
-    // Création : <DIV> à chaque itération et l'insérer dans la Section(Content)
-    let boxInContent = document.createElement('div');
-    boxInContent.classList.add('boxDish')
-    content.append(boxInContent);
+      // Création : <DIV> à chaque itération et l'insérer dans la Section(Content)
+      let boxInContent = document.createElement("div");
+      boxInContent.classList.add("boxDish");
+      content.append(boxInContent);
 
-    // Création : <FIGURE>(pour le référencement) à chaque itération et l'insérer dans la Div(boxInContent)
-    let figure = document.createElement('figure');
-    boxInContent.append(figure);
+      // Création : <FIGURE>(pour le référencement) à chaque itération et l'insérer dans la Div(boxInContent)
+      let figure = document.createElement("figure");
+      boxInContent.append(figure);
 
-    // Création : <IMAGE> à chaque itération et l'insérer dans la Div(boxInContent)
-    let imgDish = document.createElement('img');
-    imgDish.classList.add('imgDish')
-    figure.append(imgDish);
-    imgDish.style.backgroundImage = "url('" + showData.meals[0].strMealThumb + "')";
+      // Création : <IMAGE> à chaque itération et l'insérer dans la Div(boxInContent)
+      let imgDish = document.createElement("img");
+      imgDish.classList.add("imgDish");
+      figure.append(imgDish);
+      imgDish.style.backgroundImage =
+        "url('" + showData.meals[0].strMealThumb + "')";
 
+      // Changement d'image pour informer l'utilisateur que l'image est clickable
+      imgDish.addEventListener("mouseover", () => {
+        imgDish.style.backgroundImage = "";
+        imgDish.style.transition = "1.2s";
+        imgDish.style.borderRadius = "50%";
+        imgDish.style.border = "none";
+      });
 
-
-    for (const test of showData.meals) {
-      console.log(test);
+      // Reprends ses valeurs de base à la sortie de l'image
+      imgDish.addEventListener("mouseleave", () => {
+        imgDish.style.backgroundImage =
+          "url('" + showData.meals[0].strMealThumb + "')";
+        imgDish.style.transition = "1s";
+        imgDish.style.borderRadius = "0px";
+        imgDish.style.border = "1px solid #538deb";
+      });
     }
   }
 }
 
-getFetch()
-
+getFetch();
